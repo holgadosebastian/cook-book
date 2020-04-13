fe44aceea2fecead422c252a8b32a0ad0194b1d4
Sebastian Holgado <holgado.sebastian@gmail.com>
2020-04-13 21:00:09 +0200

    RECIPES: Add routes

---
+++ models/Recipe.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const RecipeSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  ingredients: {
+    type: Array,
+    required: true
+  },
+  description: {
+    type: String,
+    required: false
+  },
+  servingSize: {
+    type: Number,
+    required: false
+  },
+  cookingTime: {
+    type: Number,
+    required: false
+  },
+  creatorId: {
+    type: mongoose.Types.ObjectId,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('recipe', RecipeSchema);
--- routes/recipes.js
+++ routes/recipes.js
@@ -1,19 +1,80 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../middleware/auth');
+
+const Recipe = require('../models/Recipe');
 
 // @route     GET api/recipes
 // @desc      Get all recipes
 // @access    Private
-router.get('/', (req, res) => {
+router.get('/', auth, (req, res) => {
   res.send('Get all recipes');
+});
+
+// @route     GET api/recipes/:id
+// @desc      Get a recipe
+// @access    Private
+router.get('/:id', auth, async (req, res) => {
+  try {
+    console.log(req.params.id);
+    let recipe = await Recipe.findOne({ _id: req.params.id });
+
+    res.json({ data: recipe });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
 });
 
 // @route     POST api/recipes
 // @desc      Add new recipe
 // @access    Private
-router.post('/', (req, res) => {
-  res.send('Add recipe');
-});
+router.post(
+  '/',
+  [
+    auth,
+    check('title', 'A title for a recipe is required').not().isEmpty(),
+    check('ingredients', 'Ingredients for a recipe are required')
+      .not()
+      .isEmpty(),
+    check('servingSize', 'Serving size must be an integer').isInt(),
+    check('cookingTime', 'Cooking time must be an integer').isInt()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      title,
+      ingredients,
+      description,
+      servingSize,
+      cookingTime
+    } = req.body;
+
+    try {
+      let recipe = new Recipe({
+        title,
+        ingredients,
+        description,
+        servingSize,
+        cookingTime,
+        creatorId: req.user.id
+      });
+
+      await recipe.save();
+
+      res.json({ recipe });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 // @route     PUT api/recipes/:id
 // @desc      Update recipe
@@ -27,6 +88,20 @@ router.put('/:id', (req, res) => {
 // @access    Private
 router.delete('/:id', (req, res) => {
   res.send('Delete recipe');
+});
+
+// @route     GET api/recipes/user/:id
+// @desc      Get all recipes from a user
+// @access    Private
+router.get('/user/:id', auth, async (req, res) => {
+  try {
+    let recipes = await Recipe.find({ creatorId: req.params.id });
+
+    res.json({ data: recipes });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
 });
 
 module.exports = router;
